9249536c31e9ebc88b58e2dcf93b71e8f55a5c56
vandhana91 <85727736+vandhana91@users.noreply.github.com>
2022-04-15 12:32:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const fs= require("fs");
-const path = require('path');
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = 8080;
-
-const folderPath = './assignment/';
-const dateObj = new Date()//.toString();
-  console.log(dateObj);
-  let d = new Date();
+const fs= require("fs");
+ let d = new Date();
+ const folderPath = './assignment/';
   let filename = "" + d.getFullYear() + "-"+ d.getMonth() + "-" + d.getDate() + "-"
   + d.getTime() +'.txt'
-  const testFolder = 'assignment/';
-fs.writeFile(filename,dateObj.toString(),(err)=>{
+  fs.writeFile(filename,d.toString(),(err)=>{
     console.log("completed writing")
     
 })
 fs.readdirSync(folderPath).forEach(file => {
     console.log(file);
   });
-  app.get('/', function(req, res) {
-    
-    res.sendFile( path.resolve(filename) );
-    //res.sendFile(path.resolve(__dirname,'./assignment'));
- });
-
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send(filename)
+    .end();
+});
+ 
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
